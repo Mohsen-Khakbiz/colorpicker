3b4a62245adb85ee38f32f1b7f5e3e3ecada7587
Mohsen2500303209 <mohsen.khakbiz1@gmail.com>
2023-04-26 23:07:02 +0330

    changed button

--- src/colorpicker/ui/ColorInformation.js
+++ src/colorpicker/ui/ColorInformation.js
@@ -72,11 +72,11 @@ export default class ColorInformation extends UIElement {
                 <polyline points="6 9 12 15 18 9"></polyline>
               </svg>
             </label>
-			<button class="el-cp-clear" ref="$clear" title="Clear">
+			<span class="el-btn el-cp-clear" ref="$clear" title="Clear">
 				<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 					<polyline points="1 4 1 10 7 10"></polyline><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path>
 				</svg>
-			</button>
+			</span>
 			${ $eyedropper }
           </nav>
         </div>
